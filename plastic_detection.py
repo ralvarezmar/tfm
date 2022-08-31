81c479a0da7c496b69125a5a40d8a2c3e23d362a
Rubén <ruben.alvarez.martin@nttdata.com>
2022-08-31 22:03:25 +0200

    update algorithm

---
+++ plastic_detection.py
@@ -0,0 +1,8 @@
+import PIL
+import os
+# https://github.com/jfilter/split-folders
+import splitfolders
+import cv2
+# https://github.com/OlafenwaMoses/ImageAI
+from imageai.Detection.Custom import DetectionModelTrainer
+from imageai.Detection.Custom import CustomObjectDetection
